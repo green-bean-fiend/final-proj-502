ebb3640de5ca62573f39f6daa449384d9699b3a1
MJ <130183866+green-bean-fiend@users.noreply.github.com>
2023-05-09 23:55:33 -0400

    Update final project streamlight app mj conley DSC502.py

--- final project streamlight app mj conley DSC502.py
+++ final project streamlight app mj conley DSC502.py
@@ -6,22 +6,14 @@ import matplotlib.pyplot as plt
 st.title('Chlamydia')
 dfc1 = pd.read_csv("C:\Users\User\Documents\df1c.csv")
 
-if st.checkbox('Show Raw Data'):
-    st.subheader('Raw Data')
-    st.write(dfc1)
-
-cols = ['Indicator','Year','Geography','Pop','PopDenEST','Cases','Rate per 100000','Abst']
-
-
 df00=(df1c.loc[df1c['Abst'] == 0])
 df11=(df1c.loc[df1c['Abst'] == 1])
 
-st.markdown('---')
 st.subheader('\nSelectbox')
 option = st.selectbox(
     'Select Sexual Education Status',
     ('Abstinence- Only', 'NonAbstinence- Only'))
-st.write('You selected:', option)
+
 if option == 'Abstinence- Only'
     cases= df11['Rate per 100000']
     state= df11['Geography']
